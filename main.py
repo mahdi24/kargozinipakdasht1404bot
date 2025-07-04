b30d3aa331d27c0697724f66a03942e00f3f77a1
mahdi hassanloo <press.itengineering@gmail.com>
2025-07-04 21:31:00 +0330

    Update main.py
    Signed-off-by: mahdi hassanloo <press.itengineering@gmail.com>

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ BOT_TOKEN = "1004988187:QrErRwdnhUaKHIXjFKGxQxMHe60WUrqeGnMQz3y6"
 @app.route(f"/{BOT_TOKEN}", methods=["GET", "POST"])
 def webhook():
     if request.method == "GET":
-        return "Bot is running.", 200
+        return "Bot is running", 200
 
     data = request.get_json()
-    if not data:
-        return "no data", 400
 
     if "message" in data:
         chat_id = data["message"]["chat"]["id"]
@@ -20,21 +18,18 @@ def webhook():
 
         if text == "/start":
             reply_markup = {
-    "keyboard": [
-        [{"text": "مرخصی"}, {"text": "بازنشستگی"}, {"text": "نقل و انتقالات"}],
-        [{"text": "طبقه شغلی"}, {"text": "رتبه شغلی"}, {"text": "بازخرید"}],
-        [{"text": "استعفا"}, {"text": "تخلفات"}, {"text": "گواهی اشتغال به کار"}],
-        [{"text": "انتصابات"}, {"text": "ارتباط با ما"}]
-    ],
-    "resize_keyboard": True,
-    "one_time_keyboard": False
-}
+                "keyboard": [
+                    ["مرخصی", "بازنشستگی", "نقل و انتقالات"],
+                    ["طبقه شغلی", "رتبه شغلی", "بازخرید"],
+                    ["استعفا", "تخلفات", "گواهی اشتغال به کار"],
+                    ["انتصابات", "ارتباط با ما"]
+                ],
+                "resize_keyboard": True,
+                "one_time_keyboard": False
             }
             send_message(chat_id, "به بازوی کارگزینی اداره آموزش و پرورش پاکدشت خوش آمدید", reply_markup)
         else:
             send_message(chat_id, "دستور نامشخص است.")
-    else:
-        print("پیام نامعتبر دریافت شد:", data)
 
     return "ok", 200
 
@@ -46,7 +41,9 @@ def send_message(chat_id, text, reply_markup=None):
     }
     if reply_markup:
         payload["reply_markup"] = reply_markup
-    requests.post(url, json=payload)
+
+    response = requests.post(url, json=payload)
+    print("Response:", response.status_code, response.text)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
