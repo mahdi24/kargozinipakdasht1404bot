04b72928847daa3350c1a11bce949e2eaff70dce
mahdi hassanloo <press.itengineering@gmail.com>
2025-07-04 16:27:46 +0330

    Update main.py
    Signed-off-by: mahdi hassanloo <press.itengineering@gmail.com>

--- main.py
+++ main.py
@@ -10,28 +10,28 @@ def webhook():
     if request.method == "GET":
         return "Bot is running.", 200
 
-    update = request.get_json()
-    if "message" in update:
-        chat_id = update["message"]["chat"]["id"]
-        text = update["message"].get("text", "")
+    if request.method == "POST":
+        update = request.get_json()
+        if "message" in update:
+            chat_id = update["message"]["chat"]["id"]
+            text = update["message"].get("text", "")
 
-        if text == "/start":
-            reply_markup = {
-                "keyboard": [
-                    ["مرخصی", "بازنشستگی", "نقل و انتقالات"],
-                    ["طبقه شغلی", "رتبه شغلی", "بازخرید"],
-                    ["استعفا", "تخلفات", "گواهی اشتغال به کار"],
-                    ["انتصابات", "ارتباط با ما"]
-                ],
-                "resize_keyboard": True,
-                "one_time_keyboard": False
-            }
-            send_message(chat_id, "به بازوی کارگزینی اداره آموزش و پرورش پاکدشت خوش آمدید", reply_markup)
-        else:
-            send_message(chat_id, "دستور نامشخص است.")
+            if text == "/start":
+                reply_markup = {
+                    "keyboard": [
+                        ["مرخصی", "بازنشستگی", "نقل و انتقالات"],
+                        ["طبقه شغلی", "رتبه شغلی", "بازخرید"],
+                        ["استعفا", "تخلفات", "گواهی اشتغال به کار"],
+                        ["انتصابات", "ارتباط با ما"]
+                    ],
+                    "resize_keyboard": True,
+                    "one_time_keyboard": False
+                }
+                send_message(chat_id, "به بازوی کارگزینی اداره آموزش و پرورش پاکدشت خوش آمدید", reply_markup)
+            else:
+                send_message(chat_id, "دستور نامشخص است.")
+        return "ok", 200
 
-    return "ok", 200
-print("📥 New message:", update)
 def send_message(chat_id, text, reply_markup=None):
     url = f"https://ble.ir/api/bot{BOT_TOKEN}/sendMessage"
     payload = {
