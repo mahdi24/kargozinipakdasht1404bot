d76ada44c48f4b3124cd595c64c7be00850b945b
mahdi hassanloo <press.itengineering@gmail.com>
2025-07-07 08:53:16 +0330

    Update main.py
    Signed-off-by: mahdi hassanloo <press.itengineering@gmail.com>

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-BOT_TOKEN = os.getenv("1004988187:BOT_TOKEN")  # ✅ خواندن از متغیر Railway
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # ✅ خواندن از متغیر Railway
 API_URL = f"https://ble.ir/api/bot{BOT_TOKEN}/sendMessage"
 
 menu_buttons = [["مرخصی", "بازنشستگی"], ["نقل و انتقالات", "طبقه شغلی"], ["ارتباط با ما"]]
